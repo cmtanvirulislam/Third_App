e89103e8e2c0a0ebb251f2b74a0a84f1cc886fd8
Tanvir <engr.cmtanvir@gmail.com>
2021-03-11 18:47:35 +0600

    Fetching json data with http request

--- lib/Details.dart
+++ lib/Details.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
 class DetailsPage extends StatelessWidget {
+  var receivedata;
+  DetailsPage({this.receivedata});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-
+      child: CircleAvatar(
+        radius: 100,
+        child: Image.network(receivedata["avatar"]),
+      ),
       ),
     );
   }
