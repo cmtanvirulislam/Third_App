32a6414c06f1e91e935e096030b3f3a8b0771ee8
Tanvir <engr.cmtanvir@gmail.com>
2021-03-17 12:13:35 +0600

    Onboarding screen modified

--- lib/54. Onboarding Screen with dot indicator.dart
+++ lib/54. Onboarding Screen with dot indicator.dart
@@ -29,25 +29,39 @@ class _HomeState extends State<Home> {
         PageViewModel(
             title: "First",
             body: "This is my First Description",
-            image: Image.asset("pictures/bird.jpg",width: 350,)
-        ),
+            image: Image.asset(
+              "pictures/bird.jpg",
+              width: 350,
+            )),
         PageViewModel(
             title: "Second",
             body: "This is my Second Description",
-            image: Image.asset("pictures/food.jpg",width: 350,)
-        ),
+            image: Image.asset(
+              "pictures/food.jpg",
+              width: 350,
+            )),
         PageViewModel(
             title: "Third",
             body: "This is my Third Description",
-            image: Image.asset("pictures/sadbird.jpg",width: 350,)
-        )
+            image: Image.asset(
+              "pictures/sadbird.jpg",
+              width: 350,
+            ))
       ],
       onDone: () {
-        Navigator.push(context, CupertinoPageRoute(builder: (context) => Home1()));
+        Navigator.push(
+            context, CupertinoPageRoute(builder: (context) => Home1()));
       },
       done: Text("Done"),
       showSkipButton: true,
       skip: Text("Skip"),
+      dotsDecorator: DotsDecorator(
+        size: Size(10.0,10.0),
+        color: Colors.red,
+        activeSize: Size(22.0, 10.0),
+        activeShape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20.0))),
+      ),
     );
   }
 }
